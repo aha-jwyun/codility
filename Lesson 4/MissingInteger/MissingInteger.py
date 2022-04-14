eacd78160c9a5cdf282064b9159c6bd4faddec29
jwyun <jw.yun@a-ha.io>
2022-04-14 11:57:01 +0900

    solve_missing_integer
    solve missing integer

---
+++ Lesson 4/MissingInteger/MissingInteger.py
@@ -0,0 +1,9 @@
+def solution(A):
+    array = [0] * 100001
+    for i in A:
+        if i > 0:
+            array[i] += 1
+        
+    for i in range(1, len(array) + 1):
+        if array[i] == 0:
+            return i
