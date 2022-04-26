26ca293b7fb110942ba0ff6c6b19f62b7ff4e71d
jwyun <jw.yun@a-ha.io>
2022-04-26 10:37:56 +0900

    lesson1_solve
    lesson1_solve

--- Lesson 1/BinaryGap.py
+++ Lesson 1/BinaryGap.py
@@ -1,3 +1,32 @@
-def solution(N):
+import time
 
-    pass
+def solution(N):
+    start = time.time()
+    
+    array = []
+    answer = 0
+    count = 0
+    
+    while N >= 2:
+        namuji = N % 2
+        N //= 2
+        array.append(namuji)
+    array.append(N)
+    array.reverse()
+
+    for i in array:
+        if i == 1:
+            if count > answer:
+                answer = count
+            count = 0
+        else:
+            count += 1
+
+    print(f"{time.time() - start:.5f} sec")
+
+    return answer
+
+
+solution(1376796946)
+solution(1073741825)
+solution(1610612737)
